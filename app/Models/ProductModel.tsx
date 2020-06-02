2f3d482996d963fef4c1257bbb517a2bfb647651
Tomas Pulenta <tpulenta@hexacta.com>
2020-06-02 18:50:35 -0300

    cambio de description a observations

--- app/Models/ProductModel.tsx
+++ app/Models/ProductModel.tsx
@@ -2,16 +2,18 @@ export default class ProductModel {
   barcode: string
   material: string
   displayName: string
-  description: string
+  observations: string
   detailsId: string
   addedBy: string
+  photoUrl: string
 
   constructor() {
     this.barcode = ''
     this.material = ''
     this.displayName = ''
-    this.description = ''
+    this.observations = ''
     this.detailsId = ''
     this.addedBy = ''
+    this.photoUrl = ''
   }
 }
